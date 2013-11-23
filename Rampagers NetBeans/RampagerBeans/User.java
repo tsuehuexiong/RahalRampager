cd7c4970e772302046370461ceab5adff0db4759
Justin Springer <jespringer@csbsju.edu>
2013-11-23 12:23:57 -0600

    Tested out adding a new javabeans class

---
+++ Rampagers NetBeans/RampagerBeans/User.java
@@ -0,0 +1,41 @@
+
+
+
+public class User {
+    
+    private String username;
+    private String password;
+    private Boolean isAdmin;
+    
+    public User(){
+        
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+    
+    
+    
+    
+}
